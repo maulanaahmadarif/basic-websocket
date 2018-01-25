16595cf5ffa54df1323b4015e57f045a0e137cd4
Arif Maulana <arifmaulana@Arifs-MacBook-Pro.local>
2018-01-25 14:59:56 +0700

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -29,11 +29,12 @@ socket.on('newLocationMessage', function (message) {
 
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
+  const messageTextBox = jQuery('[name=message]');
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextBox.val()
   }, function () {
-
+    messageTextBox.val('');
   });
 })
 
@@ -44,12 +45,16 @@ locationButton.on('click', function () {
     return alert('Geolation not supported by your browser.');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longtitude: position.coords.longitude
     })
   }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location');
   })
 });
